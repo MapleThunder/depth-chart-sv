a82d39b2cab19bb8e72e10db860756d5876bb5b5
Niko Bentley <niko@maplestorm.ca>
2023-12-22 13:24:27 -0400

    initial commit of the project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import '../styles.css';
+</script>
+
+<header>
+	<a href="/"> Depth Chart </a>
+
+	<div class="clear">
+		<button>Clear</button>
+	</div>
+</header>
+
+<main><slot /></main>
+
+<footer>
+	<p>Created by Niko Bentley</p>
+</footer>
+
+<style>
+	header,
+	footer {
+		display: flex;
+		width: 100%;
+		height: 3rem;
+		align-items: center;
+		max-width: var(--column);
+		padding: 0 var(--side);
+		color: var(--accent);
+	}
+
+	header {
+		justify-content: space-between;
+	}
+
+	footer {
+		justify-content: center;
+		align-self: flex-end;
+	}
+</style>
